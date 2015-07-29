a24067579ffaf8f0e7f81d32ab5bfe5773eb42a8
narasimha99 <snmurty99@gmail.com>
2015-07-29 20:21:35 +0545

    Moidfdefifsd

--- wp-content/themes/twentyfifteen/footer.php
+++ wp-content/themes/twentyfifteen/footer.php
@@ -82,8 +82,5 @@ $url = esc_url( home_url( '/' ) );
 //		interval:   4000
 //	});
 </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <?php /*?>  <script src="js/bootstrap.min.js"></script><?php */?>
-   </body>
+</body>
 </html>
